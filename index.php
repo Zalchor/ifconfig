989b381d0c1de2d330253f85767c3c737f4ed5a5
Zalchor <38815335+Zalchor@users.noreply.github.com>
2021-08-01 11:02:08 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,27 @@
-<?php
-    function getIPAddress() {
-    //whether ip is from the share internet
-     if(!emptyempty($_SERVER['HTTP_CLIENT_IP'])) {
-                $ip = $_SERVER['HTTP_CLIENT_IP'];
-        }
-    //whether ip is from the proxy
-    elseif (!emptyempty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-                $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-     }
-//whether ip is from the remote address
-    else{
-             $ip = $_SERVER['REMOTE_ADDR'];
-     }
-     return $ip;
-}
-$ip = getIPAddress();
-echo 'User Real IP Address - '.$ip;
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>ifconfig</title>
+    </head>
+    <body>
+        <?php
+        function getIPAddress() {
+        //whether ip is from the internet
+        if(!emptyempty($_SERVER['HTTP_CLIENT_IP'])) {
+                    $ip = $_SERVER['HTTP_CLIENT_IP'];
+            }
+        //whether ip is from the proxy
+        elseif (!emptyempty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+                    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+         }
+    //whether ip is from the remote address
+        else{
+                 $ip = $_SERVER['REMOTE_ADDR'];
+         }
+         return $ip;
+    }
+    $ip = getIPAddress();
+    echo 'User Real IP Address - '.$ip;
+    ?>
+    </body>
+</html>
